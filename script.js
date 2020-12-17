c588a352acb73e53ca1d7a711c55d936f8ec0ec3
Chisom <kuzko584@gmail.com>
2020-12-17 08:46:04 +0100

    fixed carousel

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const slider = document.querySelector('.slider');
-const bannSection = document.querySelector('.banner-sec-1');
-const root = document.documentElement;
 
 
 window.addEventListener('load', () => {
-    let travel = -1 *(Number(bannSection.offsetWidth) + 4);
-    root.style.setProperty('--travel-01', travel + "px");
     slider.classList.add('animate-slider');
 });
