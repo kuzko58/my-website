0a51c51e3bcce9676871461020e19fd095bc343b
Chisom <kuzko584@gmail.com>
2020-12-17 14:25:36 +0100

    Changed UI 2.0

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const slider = document.querySelector('.slider');
+const cards = document.querySelectorAll('.card');
 
 
 window.addEventListener('load', () => {
     slider.classList.add('animate-slider');
+    cards[cards.length - 1].style.borderBottom = 'none';
 });
